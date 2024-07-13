0b62678a0645072aa6322362d6e92ab24c64f1b5
Gaby-GH <161126492+Gaby-GH@users.noreply.github.com>
2024-07-13 17:37:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,23 @@
-let div_2 = document.querySelector("#div_2")
+let div_2_templates = document.querySelector("#div_2_templates")
 
 let template_1 = document.querySelector("#temp_1")
 let template_2 = document.querySelector("#temp_2")
-div_2.innerHTML = template_1.innerHTML //<------- A REMTTRE POUR QUE LE SCRIPT FONCTIONNE
-
-let input_name = document.querySelector("#input_name")
-let input_email = document.querySelector("#input_email")
-let input_phone = document.querySelector("#input_phone")
-
-let p_red_name = document.querySelector("#p_red_name")
-let p_red_email = document.querySelector("#p_red_email")
-let p_red_phone = document.querySelector("#p_red_phone")
+let template_3 = document.querySelector("#temp_3")
+div_2_templates.innerHTML = template_3.innerHTML // REMTTRE ICI
 
 let btn_next = document.querySelector("#btn_next")
+let btn_back = document.querySelector("#btn_back")
+btn_back.setAttribute("hidden", true)
 
 let page = 1
 
 btn_next.addEventListener("click", () => {
+    console.log("next")
     if (page == 1){
-        if (input_name.value == ""){
-            input_name.style.border = "2px solid hsl(354, 84%, 57%)"
-            p_red_name.removeAttribute("hidden")
-        }else{
-            input_name.style.border = "1px solid hsl(229, 24%, 87%)"
-            p_red_name.setAttribute("hidden", true)
-        }
-    
-        if (!input_email.value.includes("@", ".")){
-            input_email.style.border = "2px solid hsl(354, 84%, 57%)"
-            p_red_email.removeAttribute("hidden")
-        }else{
-            input_email.style.border = "1px solid hsl(229, 24%, 87%)"
-            p_red_email.setAttribute("hidden", true)
-        }
-        
-        if (input_phone.value.length < 10 || (!input_phone.value.includes("1") && !input_phone.value.includes("2") && !input_phone.value.includes("3") && !input_phone.value.includes("4") && !input_phone.value.includes("5") && !input_phone.value.includes("6") && !input_phone.value.includes("7") && !input_phone.value.includes("8") && !input_phone.value.includes("9") && !input_phone.value.includes("0"))){
-            input_phone.style.border = "2px solid hsl(354, 84%, 57%)"
-            p_red_phone.removeAttribute("hidden")
-        }else{
-            input_phone.style.border = "1px solid hsl(229, 24%, 87%)"
-            p_red_phone.setAttribute("hidden", true)
-        }
-    
-    
-        if ((!input_name.value == "") && (input_email.value.includes("@", ".") && (input_phone.value.length >= 10 || (input_phone.value.includes("1") || input_phone.value.includes("2") || input_phone.value.includes("3") || input_phone.value.includes("4") || input_phone.value.includes("5") || input_phone.value.includes("6") || input_phone.value.includes("7") || input_phone.value.includes("8") || input_phone.value.includes("9") || input_phone.value.includes("0") )))){
-            page += 1
-            UpdatePage2()
-        }
+        CanPage1GoPage2()
+    }else if (page == 2){
+        UpdatePage3()
+        page += 1
     }
 })
 
@@ -55,15 +26,72 @@ let btn_2 = document.querySelector("#btn_2")
 let btn_3 = document.querySelector("#btn_3")
 let btn_4 = document.querySelector("#btn_4")
 
-function UpdatePage1(){
+function CanPage1GoPage2(){
+    let input_name = document.querySelector("#input_name")
+    let input_email = document.querySelector("#input_email")
+    let input_phone = document.querySelector("#input_phone")
 
+    let p_red_name = document.querySelector("#p_red_name")
+    let p_red_email = document.querySelector("#p_red_email")
+    let p_red_phone = document.querySelector("#p_red_phone")
+
+    if (input_name.value == ""){
+        input_name.style.border = "2px solid hsl(354, 84%, 57%)"
+        p_red_name.removeAttribute("hidden")
+    }else{
+        input_name.style.border = "1px solid hsl(229, 24%, 87%)"
+        p_red_name.setAttribute("hidden", true)
+    }
+
+    if (!input_email.value.includes("@", ".")){
+        input_email.style.border = "2px solid hsl(354, 84%, 57%)"
+        p_red_email.removeAttribute("hidden")
+    }else{
+        input_email.style.border = "1px solid hsl(229, 24%, 87%)"
+        p_red_email.setAttribute("hidden", true)
+    }
+    
+    if (input_phone.value.length < 10 || (!input_phone.value.includes("1") && !input_phone.value.includes("2") && !input_phone.value.includes("3") && !input_phone.value.includes("4") && !input_phone.value.includes("5") && !input_phone.value.includes("6") && !input_phone.value.includes("7") && !input_phone.value.includes("8") && !input_phone.value.includes("9") && !input_phone.value.includes("0"))){
+        input_phone.style.border = "2px solid hsl(354, 84%, 57%)"
+        p_red_phone.removeAttribute("hidden")
+    }else{
+        input_phone.style.border = "1px solid hsl(229, 24%, 87%)"
+        p_red_phone.setAttribute("hidden", true)
+    }
+
+
+    if ((!input_name.value == "") && (input_email.value.includes("@", ".") && (input_phone.value.length >= 10 || (input_phone.value.includes("1") || input_phone.value.includes("2") || input_phone.value.includes("3") || input_phone.value.includes("4") || input_phone.value.includes("5") || input_phone.value.includes("6") || input_phone.value.includes("7") || input_phone.value.includes("8") || input_phone.value.includes("9") || input_phone.value.includes("0") )))){
+        page += 1
+        UpdatePage2()
+    }
+}
+
+function UpdatePage1(){
+    btn_back.setAttribute("hidden", true)
+
+    btn_1.className = "btn_step_active"
+    btn_2.className = "btn_step"
+
+    div_2_templates.innerHTML = template_1.innerHTML
 }
 
 function UpdatePage2(){
     btn_1.className = "btn_step"
     btn_2.className = "btn_step_active"
 
-    div_2.innerHTML = template_2.innerHTML
+    div_2_templates.innerHTML = template_2.innerHTML
+    
+    // Mise en fonction du btn go back
+    btn_back.removeAttribute("hidden")
+    btn_back.addEventListener("click", () => {
+        if (page == 2){
+            page = 1
+            UpdatePage1()
+        }else if(page == 3){
+            page = 2
+            UpdatePage2()
+        }
+    })
 
     let arcade = document.querySelector("#arcade")
     let advanced = document.querySelector("#advanced")
@@ -145,6 +173,15 @@ function UpdatePage2(){
         }
     
     })
+}
+
+function UpdatePage3(){
+    btn_2.className = "btn_step"
+    btn_3.className = "btn_step_active"
+
+    div_2_templates.innerHTML = ""
+    console.log("page 3")
+
 }
 
 
