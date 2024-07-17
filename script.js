cf01953084c22c95de3c94a5241514312247c1be
Gaby-GH <161126492+Gaby-GH@users.noreply.github.com>
2024-07-17 12:46:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let template_2 = document.querySelector("#temp_2")
 let template_3 = document.querySelector("#temp_3")
 let template_4 = document.querySelector("#temp_4")
 let template_5 = document.querySelector("#temp_5")
-div_2_templates.innerHTML = template_5.innerHTML 
+div_2_templates.innerHTML = template_1.innerHTML 
 
 let data = {
     personal_infos : {
